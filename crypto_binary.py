3cfd9705fe16db85846e6b52b82840c95596a11a
ARodriguez05 <72410925+ARodriguez05@users.noreply.github.com>
2021-06-08 17:05:47 +0200

    Update neural network
    Implementation of a convolutional layer in the classic neural network

--- crypto_binary.py
+++ crypto_binary.py
@@ -1,5 +1,6 @@
-import os
+import os, logging
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+logging.disable(logging.WARNING)
 
 import tensorflow as tf
 import cirq
@@ -9,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import time
+import math
 
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
@@ -25,35 +27,58 @@ implémenté. Etudier la possibilité de modifier sur-mesures le layer si néces
 
 [*] Modifier les réseaux de neurones pour minimiser la perte (le réseau quantique est particulièrement
 inefficace pour un décryptage 8-bits => à voir /!\).
-    * Simulation 2 bits :
+    * Simulation 2 bits [10 epochs] :
         - Réseau classique :
-            # learning_rate = 0.025 | 0.08 | 0.1
+            # learning_rate = 0.025 | [0.08] (??%) | 0.1
             # layers = Dense
         - Réseau quantique :
-            # learning_rate = 0.05
+            # learning_rate = 0.05 (100%)
+            # layers = QuantumLayer (6)
+
+    * Simulation 4 bits :
+        - Réseau classique :
+            # learning_rate = 0.08 (85%) [50 epochs], 0.09 (87.5%) [100 epochs], 0.05 (65%~100%) [100 epochs], 0.03 (82%~90%) [100 epochs]
+            # layers = Dense
+        - Réseau quantique :
+            # learning_rate = 0.05 (75%)
             # layers = QuantumLayer
 
 [*] Faire une démo optimisée de l'apprentissage du XOR 8-bits pour comparer théorie classique et quantique
 + chiffrer temporellement l'efficacité des solutions.
 
-[*] Penser la phase 2 du projet : cryptage sur 16/32 bits ? Conversion textuelle directe ?
+[*] Penser la phase 2 du projet : cryptage sur 16/32 bits, conversion textuelle directe...
+
+
+[*****] Voir comment implémenter la superposition avec pennylane + Ajouter un convolutional layer pour le classique [*****]
 """
 
 
 # ---------------------------- Classes --------------------------------- #
 class NeuralNetwork:
-    def __init__(self, xs_data, ys_data, sample_size=8, number=None):
+    def __init__(self, xs_data, ys_data, sample_size=8, number=None, convolutional=0):
         self.name = None
         if number!=None:
             self.name = "Neural Network n°"+str(number)
         else:
             self.name = "Neural Network"
-        self.xs_data = xs_data
-        self.ys_data = ys_data
-        self.model = tf.keras.Sequential([
-        tf.keras.layers.Dense(units=sample_size, input_shape=[sample_size])
-        ])
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.08) #0.08 Tx = 75%
+        self.input_shape = (len(xs_data), 1, sample_size)
+        self.convolutional = convolutional
+        self.total_accuracy = 0.0
+        model = list([])
+
+        if convolutional:
+            self.name = "Convolutional "+self.name
+            self.xs_data = tf.reshape(xs_data, self.input_shape)
+            self.ys_data = tf.reshape(ys_data, self.input_shape)
+            model.append(tf.keras.layers.Conv1D(filters=8, kernel_size=1, strides=1, input_shape=self.input_shape))
+        else :
+            self.xs_data = xs_data
+            self.ys_data = ys_data
+
+        model.append(tf.keras.layers.Dense(units=sample_size, input_shape=[sample_size]))
+        self.model = tf.keras.Sequential(model)
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.03)
         self.loss = tf.keras.losses.MeanSquaredError()
         self.model.compile(optimizer=self.optimizer, loss=self.loss)
 
@@ -62,17 +87,28 @@ class NeuralNetwork:
 
     def prediction(self, bits_table):
         print("\n\n*-----Prediction of "+str(bits_table)+" -----*")
-        result = self.model.predict([bits_table])
+        result = None
+        if self.convolutional :
+            result = self.model.predict(tf.reshape(bits_table, (1, 1, len(bits_table))))
+        else :
+            result = self.model.predict([bits_table])
         accurate_value = encrypt(bits_table)
         print("Predicted value : "+str(result[0])+". The correct value is : "+str(accurate_value))
 
         array = list([])
         for table in result:
             for bit in table:
-                value = int(round(bit))
-                if(value<0):
-                    value=0
-                array+=[value]
+                if self.convolutional :
+                    for extract in bit:
+                        value = int(round(extract))
+                        if(value<0):
+                            value=0
+                        array.append(value)
+                else :
+                    value = int(round(bit))
+                    if(value<0):
+                        value=0
+                    array.append(value)
 
         accuracy = 0.0
         for i in range(len(bits_table)):
@@ -81,10 +117,52 @@ class NeuralNetwork:
                 accuracy = accuracy + 1
 
         accuracy = (accuracy/len(bits_table))*100
-        print("Accuracy : "+str(accuracy)+"%") #Rates à modifier pour avoir un apprentissage efficace
+        print("Accuracy : "+str(accuracy)+"%")
+
+    def evaluateAccuracyOnSample(self, sample):
+        result = None
+        if self.convolutional :
+            result = self.model.predict(tf.reshape(sample, (1, 1, len(sample))))
+        else :
+            result = self.model.predict([sample])
+        accurate_value = encrypt(sample)
+
+        array = list([])
+        for table in result:
+            for bit in table:
+                if self.convolutional :
+                    for extract in bit:
+                        value = int(round(extract))
+                        if(value<0):
+                            value=0
+                        array.append(value)
+                else :
+                    value = int(round(bit))
+                    if(value<0):
+                        value=0
+                    array.append(value)
+
+        accuracy = 0.0
+        for i in range(len(sample)):
+            if array[i]==accurate_value[i]:
+                accuracy = accuracy + 1
+
+        accuracy = (accuracy/len(sample))*100
+        return accuracy
+
+    def evaluateTotalAccuracy(self):
+        self.total_accuracy = 0
+        number_of_samples = len(self.xs_data)*len(self.xs_data)
+        for i in range(0, number_of_samples, 1):
+            sample = convertToByte(i, len(self.xs_data))
+            self.total_accuracy += self.evaluateAccuracyOnSample(sample)
+        self.total_accuracy/=number_of_samples
+        return self.total_accuracy
+
+
 
 class QuantumNeuralNetwork(NeuralNetwork):
-    def __init__(self, xs_data, ys_data, number_of_qubits=8, number=None):
+    def __init__(self, xs_data, ys_data, number_of_qubits=8, number=None, convolutional=0):
         self.name = None
         if number!=None:
             self.name = "Quantum Neural Network n°"+str(number)
@@ -92,73 +170,45 @@ class QuantumNeuralNetwork(NeuralNetwork):
             self.name = "Quantum Neural Network"
         self.xs_data = xs_data
         self.ys_data = ys_data
+        self.convolutional = convolutional
 
         #Quantum Layer creation
         dev = qml.device("default.qubit", wires=number_of_qubits)
         @qml.qnode(dev)
         def qnode(inputs, weights):
             qml.templates.AngleEmbedding(inputs, wires=range(number_of_qubits))
+            #qml.templates.StronglyEntanglingLayers(weights, wires=range(number_of_qubits))
             qml.templates.BasicEntanglerLayers(weights, wires=range(number_of_qubits))
             return [qml.expval(qml.PauliZ(wires=i)) for i in range(number_of_qubits)]
-        n_layers = 6
+        n_layers = 6 # 1 layer => 71% (15sec) | 10 layers => 50% (88sec)
         weight_shapes = {"weights": (n_layers, number_of_qubits)}
         Quantum_Layer = qml.qnn.KerasLayer(qnode, weight_shapes, output_dim=number_of_qubits)
 
 
         self.model = tf.keras.Sequential([
-        #tf.keras.layers.Dense(units=number_of_qubits, input_shape=[number_of_qubits]),
         Quantum_Layer,
-        #tf.keras.layers.Dense(number_of_qubits, activation='relu')
         ])
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.065)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.005)
         self.model.compile(optimizer=self.optimizer, loss=tf.keras.losses.MeanSquaredError())
 
-#Not used
-class Quantum(Layer):
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
-        super(Quantum, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        self.kernel = self.add_weight(name = 'kernel', shape = (input_shape[1], self.output_dim), initializer = 'normal', trainable = True)
-        super(Quantum, self).build(input_shape)
-
-    def call(self, input_data):
-        data = quantumMapping(tf.convert_to_tensor(input_data))
-        return K.dot(data, self.kernel)
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim) # Implémentation : Quantum(units, input_shape=[size])
-
-
-# --------------------- Tests custom loss function --------------------- #
-def mse(y_true, y_pred, hidden):
-    error = y_true-y_pred
-    return K.mean(K.square(error)) + K.mean(hidden)
-
-def test():
-    X = np.random.uniform(0,1, (1000,10))
-    y = np.random.uniform(0,1, 1000)
-
-    inp = Input((10,))
-    true = Input((1,))
-    x1 = Dense(32, activation='relu')(inp)
-    x2 = Dense(16, activation='relu')(x1)
-    out = Dense(1)(x2)
-
-    m = Model([inp,true], out)
-    m.add_loss( mse( true, out, x1 ) )
-    m.compile(loss=None, optimizer='adam')
-    m.fit(x=[X, y], y=None, epochs=3)
-
-    ## final fitted model to compute predictions
-    final_m = Model(inp, out)
 
 # ---------------------------------------------------------------------- #
 def generateByteArray(array_length):
     byte_array = list([])
     for i in range(0, array_length, 1):
         byte_array.append = random.randint(0,1)
+    return byte_array
+
+def convertToByte(my_integer, byte_length):
+    byte_array = list([])
+    for i in range(0, byte_length, 1):
+        byte_array.append(0)
+    pow_2_substract = 64
+    while(my_integer>0):
+        if(my_integer-pow_2_substract >= 0):
+            my_integer-=pow_2_substract
+            byte_array[int(math.log(pow_2_substract,2))] = 1
+        pow_2_substract/=2
     return byte_array
 
 def encrypt(bytes_array):
@@ -168,30 +218,6 @@ def encrypt(bytes_array):
         encrypted_array.append((bytes_array[i]+key_array[i])%2)
     return encrypted_array
 
-#Not used
-def quantumMapping(bytes_array):
-    circuit = cirq.Circuit()
-    qubits = cirq.LineQubit.range(8) #len(bytes_array)
-    sim = cirq.Simulator()
-
-    for i in range(len(bytes_array)):
-        if bytes_array[i] == 1:
-            circuit.append([cirq.X(qubits[i])])
-            #circuit.append([cirq.Z(qubits[i])])
-        circuit.append([cirq.measure(qubits[i])])
-    print(circuit)
-
-    #Get state_vector after mapping
-    results = sim.simulate(circuit)
-    print(results)
-    samples = cirq.sample_state_vector(results.state_vector(), indices=[0, 1, 2, 3, 4, 5, 6, 7])
-    print(samples[0])
-    return samples[0]
-
-#Not used
-def quantumLossFunction(y, y_predicted):
-    return tf.reduce_sum(tf.square(tf.subtract(y, y_predicted)))
-
 def setSampleArrays(sample_size, batch_size=-1):
     xs = None
     ys = None
@@ -199,8 +225,8 @@ def setSampleArrays(sample_size, batch_size=-1):
         batch_size = sample_size
 
     if sample_size == 2 and batch_size==sample_size:
-        xs = np.array([ encrypt([0, 0]), encrypt([1, 1]) ], dtype=int) #Known encrypted values : 1,0 | 0,1
-        ys = np.array([ [0, 0], [1, 1] ], dtype=int)
+        xs = np.array([ encrypt([0, 0]), encrypt([0, 1]) ], dtype=int) #Known encrypted values : 1,1 | 0,0
+        ys = np.array([ [0, 0], [0, 1] ], dtype=int)
 
     if sample_size == 4 and batch_size==sample_size:
         xs = np.array([ encrypt([0,0,0,0]), encrypt([0,1,1,1]), encrypt([0,1,0,1]), encrypt([1,0,1,0]) ], dtype=int)
@@ -242,12 +268,11 @@ def Quantum_Main():
     quantumNetwork.prediction(bits_table)
     return 0
 
-
 def Main():
     print("\n\n\n--------------------------- Quantum Machine Learning ---------------------------\n\n")
     loop_boolean = 1
     total_epoch = 0
-    number_epochs_training = 50
+    number_epochs_training = 100
     networks = list([])
 
     nb_networks = int(input("Choose how many neural networks you want to create for this simulation : "))
@@ -269,7 +294,10 @@ def Main():
             print("/!\ You did not entered a valid number. Please enter either \"1\" or \"2\". /!\ ")
             neural_network_type = int(input("Choose your type of neural network.\n   1. Classic Neural Network\n   2. Quantum Neural Network\n\nYour choice : "))
         if neural_network_type==1:
-            networks.append(NeuralNetwork(xs, ys, sample_size, i+1))
+            is_convolutional = int(input("\nShould the network be convolutional ? 1 = yes, 0 = no.\nYour input : "))
+            if is_convolutional != 1 and is_convolutional != 0 :
+                is_convolutional = 0
+            networks.append(NeuralNetwork(xs, ys, sample_size, i+1, is_convolutional))
         else:
             networks.append(QuantumNeuralNetwork(xs, ys, sample_size, i+1))
 
@@ -282,10 +310,11 @@ def Main():
         print("["+network.name+"] Training time : "+str(float(int(time_elapsed*1000))/1000)+"s")
 
     while(loop_boolean):
-        user_choice = int(input("\n[*] Main menu : \n   [1]. Test the models on a specific entry.\n   [2]. Train the models ("+str(number_epochs_training)+" epochs).\n   [3]. View loss function variation.\n   [4]. Quit the simulation.\n\nYour choice : "))
-        while(user_choice != 1 and user_choice != 2 and user_choice != 3 and user_choice != 4):
+        string_menu = "\n[*] Main menu : \n   [1]. Test the models on a specific entry.\n   [2]. Train the models ("+str(number_epochs_training)+" epochs).\n   [3]. View loss function variation.\n   [4]. Evaluate the accuracy of each network.\n   [5]. Quit the simulation.\n\nYour choice : "
+        user_choice = int(input(string_menu))
+        while(user_choice != 1 and user_choice != 2 and user_choice != 3 and user_choice != 4 and user_choice != 5):
             print("/!\ You did not entered a valid number. Please enter either \"1\", \"2\", \"3\" or \"4\". /!\ ")
-            user_choice = int(input("\n[*] Main menu : \n   [1]. Test the models on a specific entry.\n   [2]. Train the models ("+str(number_epochs_training)+" epochs).\n   [3]. Quit the simulation.\n\nYour choice : "))
+            user_choice = int(input(string_menu))
 
         if user_choice == 1:
             user_input = input("\nEnter an array to try (format : 1,0,1,1,1,0,0...) : ")
@@ -324,27 +353,20 @@ def Main():
             plt.show(block=False)
 
         if user_choice == 4:
+            print()
+            for network in networks:
+                print("Total accuracy ["+network.name+"] : "+str(network.evaluateTotalAccuracy())+"%")
+            garbage_collector = input("\n----- Press enter to continue. -----\n")
+
+        if user_choice == 5:
             print("\n----- Exit -----\n")
             loop_boolean = 0
 
 
-# -----------------------Quantum Layer initialization------------------- #
-"""
-np.random.seed(1234)
-n_qubits = 2
 
-dev = qml.device("default.qubit", wires=n_qubits)
-@qml.qnode(dev)
-def qnode(inputs, weights):
-    qml.templates.AngleEmbedding(inputs, wires=range(n_qubits))
-    qml.templates.BasicEntanglerLayers(weights, wires=range(n_qubits))
-    return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
-n_layers = 6
-weight_shapes = {"weights": (n_layers, n_qubits)}
-Quantum_Layer = qml.qnn.KerasLayer(qnode, weight_shapes, output_dim=n_qubits)
-"""
 
 Main()
+
 
 #
 #
